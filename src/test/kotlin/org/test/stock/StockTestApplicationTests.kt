d11b4d60d9275a23d46e305de9b9483f1e349015
jeongdalma <jeongdalma@gmail.com>
2025-10-10 14:46:18 +0900

    init

---
+++ src/test/kotlin/org/test/stock/StockTestApplicationTests.kt
@@ -0,0 +1,12 @@
+package org.test.stock
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StockTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
